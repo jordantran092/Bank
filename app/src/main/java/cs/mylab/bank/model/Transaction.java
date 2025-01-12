250cd813f66dada8adcc7e623d06444ed5ec2c45
tran-jordan <jordantran092@gmail.com>
2025-01-11 20:40:26 -0500

    Initial commit

---
+++ app/src/main/java/cs/mylab/bank/model/Transaction.java
@@ -0,0 +1,20 @@
+package cs.mylab.bank.model;
+
+public class Transaction {
+
+    private String transactionType;
+    private double amount;
+
+    public Transaction(String transactionType, double amount) {
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
+
+    /*
+     * Returns status of a transaction in terms of transaction type and amount
+     */
+    public String getStatus() {
+        return String.format("Transaction %s: $%.2f", this.transactionType, this.amount);
+    }
+
+}
