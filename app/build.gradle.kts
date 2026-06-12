bec466cd21f435ba929f6a237368ddd00c95d508
tran-jordan <jordantran092@gmail.com>
2026-06-12 12:08:12 -0400

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.jackson.databind)
+    implementation(libs.lombok)
+    implementation(libs.okhttp)
 }
--- app/src/main/java/com/jordantran/bank/MainActivity.java
+++ app/src/main/java/com/jordantran/bank/MainActivity.java
@@ -7,26 +7,28 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import com.jordantran.bank.model.Bank;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
-    Declare bank object to be shared between the controller methods upon user interaction during button clicks at runtime.
-     */
-    Bank bank;
+    ObjectMapper objectMapper;
+
+    public MainActivity() {
+        this.objectMapper = new ObjectMapper();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        Initializes the bank object and displays the current bank status
-         */
-        bank = new Bank();
-        setContentsOfTextView(R.id.outputStatus, bank.getStatus());
+
+        setContentsOfTextView(R.id.outputStatus, getStatus());
     }
 
     /* Sets the content of a text label */
@@ -52,64 +54,84 @@ public class MainActivity extends AppCompatActivity {
         return string;
     }
 
+    /* Helper Methods */
 
-    /*
-    When add a new account is pressed, will retrieve the name and initial balance and use that to create a new client and then output the status of the bank
-     */
-    public void computeButtonAddNewAccountClicked(View view) {
-        String name = getInputOfTextField(R.id.inputClientName);
-        double amount = Double.parseDouble(getInputOfTextField(R.id.inputStartBalance));
-        bank.addClient(name, amount);
-        setContentsOfTextView(R.id.outputStatus, bank.getStatus());
+    private String getStatus() {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://jordantran-bookapi.k9hqccrxv6fxw.ca-central-1.cs.amazonlightsail.com/api/v1/bank/status")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String responseJson = response.body().string();
+
+            BankStatusDTO bankStatusDTO = objectMapper.readValue(responseJson, BankStatusDTO.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //return
     }
 
-    /*
-    Performs the service depending on what service chosen in service type drop down menu.
-        - If deposit, will get input amount and input name to deposit into
-        - If withdraw, will get input amount and input name to withdraw from
-        - If transfer, will get input names and input amount to transfer
-        - If print statement, will get input name and print the client's statement
-        - If an error occurs, status of bank will output the appropriate error
-     */
-    public void computeButtonConfirmClicked(View view) {
-        String optionSelected = getItemSelected(R.id.optionsServiceType);
-        double amount = 0;
-        String output = "";
 
-        if(!optionSelected.equals("Print Statement")) {
-            amount = Integer.parseInt(this.getInputOfTextField(R.id.inputAmount));
-        }
-
-        if(optionSelected.equals("Deposit")) {
-            String name = this.getInputOfTextField(R.id.inputToAccount);
-            bank.deposit(name, amount);
-        }
-        else if(optionSelected.equals("Withdraw")) {
-            String name = this.getInputOfTextField(R.id.inputFromAccount);
-            bank.withdraw(name, amount);
-        }
-        else if(optionSelected.equals("Transfer")) {
-            String fromName = this.getInputOfTextField(R.id.inputFromAccount);
-            String toName = this.getInputOfTextField(R.id.inputToAccount);
-            bank.transfer(fromName, toName, amount);
-        }
-        else if(optionSelected.equals("Print Statement")) {
-            String name = this.getInputOfTextField(R.id.inputFromAccount);
-
-            String[] statement = bank.getStatement(name);
-            for(int i = 0; i < statement.length; i++) {
-                output += String.format("%s", statement[i]);
-                if (i < statement.length - 1) {
-                    output += "\n";
-                }
-            }
-        }
-
-
-        if(!optionSelected.equals("Print Statement")) {
-            output = bank.getStatus();
-        }
-        setContentsOfTextView(R.id.outputStatus, output);
-    }
+//    /*
+//    When add a new account is pressed, will retrieve the name and initial balance and use that to create a new client and then output the status of the bank
+//     */
+//    public void computeButtonAddNewAccountClicked(View view) {
+//        String name = getInputOfTextField(R.id.inputClientName);
+//        double amount = Double.parseDouble(getInputOfTextField(R.id.inputStartBalance));
+//        bank.addClient(name, amount);
+//        setContentsOfTextView(R.id.outputStatus, bank.getStatus());
+//    }
+//
+//    /*
+//    Performs the service depending on what service chosen in service type drop down menu.
+//        - If deposit, will get input amount and input name to deposit into
+//        - If withdraw, will get input amount and input name to withdraw from
+//        - If transfer, will get input names and input amount to transfer
+//        - If print statement, will get input name and print the client's statement
+//        - If an error occurs, status of bank will output the appropriate error
+//     */
+//    public void computeButtonConfirmClicked(View view) {
+//        String optionSelected = getItemSelected(R.id.optionsServiceType);
+//        double amount = 0;
+//        String output = "";
+//
+//        if(!optionSelected.equals("Print Statement")) {
+//            amount = Integer.parseInt(this.getInputOfTextField(R.id.inputAmount));
+//        }
+//
+//        if(optionSelected.equals("Deposit")) {
+//            String name = this.getInputOfTextField(R.id.inputToAccount);
+//            bank.deposit(name, amount);
+//        }
+//        else if(optionSelected.equals("Withdraw")) {
+//            String name = this.getInputOfTextField(R.id.inputFromAccount);
+//            bank.withdraw(name, amount);
+//        }
+//        else if(optionSelected.equals("Transfer")) {
+//            String fromName = this.getInputOfTextField(R.id.inputFromAccount);
+//            String toName = this.getInputOfTextField(R.id.inputToAccount);
+//            bank.transfer(fromName, toName, amount);
+//        }
+//        else if(optionSelected.equals("Print Statement")) {
+//            String name = this.getInputOfTextField(R.id.inputFromAccount);
+//
+//            String[] statement = bank.getStatement(name);
+//            for(int i = 0; i < statement.length; i++) {
+//                output += String.format("%s", statement[i]);
+//                if (i < statement.length - 1) {
+//                    output += "\n";
+//                }
+//            }
+//        }
+//
+//
+//        if(!optionSelected.equals("Print Statement")) {
+//            output = bank.getStatus();
+//        }
+//        setContentsOfTextView(R.id.outputStatus, output);
+//    }
 
 }
